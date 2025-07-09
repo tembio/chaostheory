2fd58892f8b265a073f82cd31a89ab2999af47f8
diegomoral <diego.moral@globalsign.com>
2025-07-09 23:18:52 +0100

    clean up main

--- leaderboard/main.go
+++ leaderboard/main.go
@@ -7,41 +7,32 @@ import (
 )
 
 func main() {
-	// comps := []Competition{
-	// 	{
-	// 		ID:        1,
-	// 		Name:      "Monthly Challenge",
-	// 		ScoreRule: "event_type=='bet' && distributor=='evo' ? amount : 0",
-	// 		StartTime: "2023-10-01T00:00:00Z",
-	// 		EndTime:   "2023-10-31T23:59:59Z",
-	// 		Rewards:   []string{"Gold Medal", "Silver Medal", "Bronze Medal"},
-	// 	},
-	// 	{
-	// 		ID:        2,
-	// 		Name:      "Weekly Sprint",
-	// 		ScoreRule: "event_type=='bet' && game=='Poker' ? amount : 0",
-	// 		StartTime: "2023-10-01T00:00:00Z",
-	// 		EndTime:   "2023-10-31T23:59:59Z",
-	// 		Rewards:   []string{"Gold Medal", "Silver Medal", "Bronze Medal"},
-	// 	},
-	// }
+	comps := []Competition{
+		{
+			ID:        1,
+			Name:      "Monthly Challenge",
+			ScoreRule: "event_type=='bet' && distributor=='evo' ? amount : 0",
+			StartTime: "2023-10-01T00:00:00Z",
+			EndTime:   "2023-10-31T23:59:59Z",
+			Rewards:   []string{"Gold Medal", "Silver Medal", "Bronze Medal"},
+		},
+		{
+			ID:        2,
+			Name:      "Weekly Sprint",
+			ScoreRule: "event_type=='bet' && game=='Poker' ? amount : 0",
+			StartTime: "2023-10-01T00:00:00Z",
+			EndTime:   "2023-10-31T23:59:59Z",
+			Rewards:   []string{"Gold Medal", "Silver Medal", "Bronze Medal"},
+		},
+	}
 
-	// betEvent := common.BetEvent{
-	// 	EventID:      1,
-	// 	EventType:    "bet",
-	// 	UserID:       42,
-	// 	Amount:       100.0,
-	// 	Currency:     "USD",
-	// 	ExchangeRate: 1.0,
-	// 	Game:         "Poker",
-	// 	Distributor:  "evo",
-	// 	Studio:       "StudioX",
-	// 	Timestamp:    "2023-10-01T12:00:00Z",
-	// }
 	fmt.Println("Leaderboard service started")
 
 	ruleEvaluator := &BetRuleEvaluator{}
 	leaderboard := NewLeaderboard(ruleEvaluator)
+	// Register competitions in the leaderboard
+	leaderboard.RegistrerCompetition(&comps[0])
+	leaderboard.RegistrerCompetition(&comps[1])
 
 	// Initialize SQLiteScoreRepository
 	dbPath := "leaderboard.db"
@@ -97,12 +88,6 @@ func main() {
 	for {
 		<-stop // Block until an event is received
 	}
-
-	// 	var userEvent common.UserEvent
-	// if err := json.Unmarshal(body, &userEvent); err == nil {
-	// 	fmt.Printf("Received UserEvent: %+v\n", userEvent)
-	// 	return nil
-	// }
 
 	// Register competitions in the leaderboard
 	// leaderboard.RegistrerCompetition(&comps[0])
