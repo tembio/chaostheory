6124fa41545bbd41b6a62379199f86d6976aa3ec
diegomoral <diego.moral@globalsign.com>
2025-07-13 16:03:53 +0100

    used parameterised host

--- build_k8s.sh
+++ build_k8s.sh
@@ -6,8 +6,10 @@ eval $(minikube docker-env)
 
 ./mockeventgenerator/build.sh
 ./leaderboard/build.sh
+docker build -t frontend ./frontend
 
 kubectl apply -f k8s/leaderboard-pvc.yaml
 kubectl apply -f k8s/rabbitmq-deployment.yaml
 kubectl apply -f k8s/leaderboard-deployment.yaml
 kubectl apply -f k8s/mockeventgenerator-deployment.yaml
+kubectl apply -f k8s/frontend-deployment.yaml
